574efb8eb2e18bf5bb73a16954ed4bbb3f6359ee
Ngakezzy <ngakezzy@gmail.com>
2023-11-14 18:39:54 +0700

    thay buider bằng listener

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -12,93 +12,93 @@ class LoginScreen extends StatelessWidget {
   final TextEditingController userNameController = TextEditingController();
 
   final TextEditingController passwordController = TextEditingController();
-  void login() {}
+
   @override
   Widget build(BuildContext context) {
     final LoginBloc blocRead = context.read<LoginBloc>();
     return Scaffold(
       body: SingleChildScrollView(
-        child: Center(child:
-            BlocBuilder<LoginBloc, LoginState>(builder: (context, state) {
-          return Column(
-            children: [
-              const SizedBox(
-                height: 100,
-              ),
-              const Text(
-                'Login',
-                style: TextStyle(color: Colors.green, fontSize: 50),
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              CustomTextfieldLogin(
-                  labelText: 'username', controller: userNameController),
-              const SizedBox(
-                height: 30,
-              ),
-              CustomTextfieldLogin(
-                  labelText: 'password', controller: passwordController),
-              const SizedBox(
-                height: 20,
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.7,
-                height: 40,
-                child: ElevatedButton(
-                  onPressed: () {
-                    blocRead.add(AuthLogin(
-                        userNameController.text, passwordController.text));
-                    blocRead.add(const CheckUser());
-
-                    state.isLogin
-                        ? Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const HomeApp()))
-                        : ShowAlertDialog(
-                            context: context,
-                            title: 'Tài khoản mật khẩu không chính xác',
-                            content: 'Xác nhận để thoát',
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                          );
-
-                    userNameController.text = '';
-                    passwordController.text = '';
-                  },
-                  child: const Text('Login'),
-                ),
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              Padding(
-                // width: MediaQuery.of(context).size.width * 0.8,
-                padding: EdgeInsets.symmetric(
-                    horizontal: MediaQuery.of(context).size.width * 0.1),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text('Bạn chưa có tài khoản ?'),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.push(
+        child: Center(
+            child: BlocListener<LoginBloc, LoginState>(
+                listener: (context, state) {
+                  state.isLogin
+                      ? Navigator.push(
                           context,
                           MaterialPageRoute(
-                            builder: (context) => const RegisterScreen(),
-                          ),
+                              builder: (context) => const HomeApp()))
+                      : ShowAlertDialog(
+                          context: context,
+                          title: 'Tài khoản mật khẩu không chính xác',
+                          content: 'Xác nhận để thoát',
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
                         );
-                      },
-                      child: const Text('Đăng ký'),
+                },
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 100,
+                    ),
+                    const Text(
+                      'Login',
+                      style: TextStyle(color: Colors.green, fontSize: 50),
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    CustomTextfieldLogin(
+                        labelText: 'username', controller: userNameController),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    CustomTextfieldLogin(
+                        labelText: 'password', controller: passwordController),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.7,
+                      height: 40,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          blocRead.add(AuthLogin(userNameController.text,
+                              passwordController.text));
+                          blocRead.add(const CheckUser());
+
+                          userNameController.text = '';
+                          passwordController.text = '';
+                        },
+                        child: const Text('Login'),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    Padding(
+                      // width: MediaQuery.of(context).size.width * 0.8,
+                      padding: EdgeInsets.symmetric(
+                          horizontal: MediaQuery.of(context).size.width * 0.1),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text('Bạn chưa có tài khoản ?'),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const RegisterScreen(),
+                                ),
+                              );
+                            },
+                            child: const Text('Đăng ký'),
+                          )
+                        ],
+                      ),
                     )
                   ],
-                ),
-              )
-            ],
-          );
-        })),
+                ))),
       ),
     );
   }
--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -19,82 +19,81 @@ class RegisterScreen extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Center(
-          child: BlocBuilder<RegisterBloc, RegisterState>(
-            builder: (context, state) {
-              return Column(
-                children: [
-                  const SizedBox(
-                    height: 100,
-                  ),
-                  const Text(
-                    'Register',
-                    style: TextStyle(color: Colors.green, fontSize: 50),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  CustomTextfieldLogin(
-                      labelText: 'username', controller: userNameController),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  CustomTextfieldLogin(
-                      labelText: 'password', controller: passwordController),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  CustomTextfieldLogin(
-                      labelText: 'confirm password',
-                      controller: confirmPasswordController),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.7,
-                      height: 40,
-                      child: ElevatedButton(
-                          onPressed: () {
-                            BlocProvider.of<RegisterBloc>(context).add(
-                              RegisterCheck(
-                                newUsername: userNameController.text,
-                                newPassword: passwordController.text,
-                                newConfirmPassword:
-                                    confirmPasswordController.text,
-                              ),
-                            );
-
-                            state.isRegister
-                                ? ShowAlertDialog(
-                                    context: context,
-                                    title: 'Đăng ký thành công',
-                                    content: 'Nhấn ok để đăng nhập',
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) => LoginScreen(),
-                                        ),
-                                      );
-                                    },
-                                  )
-                                : ShowAlertDialog(
-                                    context: context,
-                                    title: 'Lỗi đăng ký',
-                                    content:
-                                        'Vui lòng kiểm tra điều kiện của username và password.',
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                    },
-                                  );
-                            userNameController.text = '';
-                            passwordController.text = '';
-                            confirmPasswordController.text = '';
-                          },
-                          child: const Text('Register')))
-                ],
-              );
+          child: BlocListener<RegisterBloc, RegisterState>(
+            listener: (context, state) {
+              state.isRegister
+                  ? ShowAlertDialog(
+                      context: context,
+                      title: 'Đăng ký thành công',
+                      content: 'Nhấn ok để đăng nhập',
+                      onPressed: () {
+                        Navigator.pop(context);
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => LoginScreen(),
+                          ),
+                        );
+                      },
+                    )
+                  : ShowAlertDialog(
+                      context: context,
+                      title: 'Lỗi đăng ký',
+                      content:
+                          'Vui lòng kiểm tra điều kiện của username và password.',
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    );
             },
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 100,
+                ),
+                const Text(
+                  'Register',
+                  style: TextStyle(color: Colors.green, fontSize: 50),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                CustomTextfieldLogin(
+                    labelText: 'username', controller: userNameController),
+                const SizedBox(
+                  height: 20,
+                ),
+                CustomTextfieldLogin(
+                    labelText: 'password', controller: passwordController),
+                const SizedBox(
+                  height: 20,
+                ),
+                CustomTextfieldLogin(
+                    labelText: 'confirm password',
+                    controller: confirmPasswordController),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.7,
+                    height: 40,
+                    child: ElevatedButton(
+                        onPressed: () {
+                          BlocProvider.of<RegisterBloc>(context).add(
+                            RegisterCheck(
+                              newUsername: userNameController.text,
+                              newPassword: passwordController.text,
+                              newConfirmPassword:
+                                  confirmPasswordController.text,
+                            ),
+                          );
+                          userNameController.text = '';
+                          passwordController.text = '';
+                          confirmPasswordController.text = '';
+                        },
+                        child: const Text('Register')))
+              ],
+            ),
           ),
         ),
       ),
